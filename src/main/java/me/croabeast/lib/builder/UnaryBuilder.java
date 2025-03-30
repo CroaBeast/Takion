5fcbe2d20349cc6acd19b52ffb596b687ac97933
CroaBeast <themastercode55@gmail.com>
2025-03-29 21:00:23 -0500

    1.0 - Added Builder implementations

---
+++ src/main/java/me/croabeast/lib/builder/UnaryBuilder.java
@@ -0,0 +1,33 @@
+package me.croabeast.lib.builder;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+/**
+ * Represents a builder that holds a single value of type {@code T} and allows modifications.
+ *
+ * @param <T> the type of value stored by the builder
+ * @param <B> the specific builder type extending this interface
+ */
+public interface UnaryBuilder<T, B extends UnaryBuilder<T, B>> extends Builder<B> {
+
+    /**
+     * Retrieves the stored value.
+     *
+     * @return the current value
+     */
+    T getValue();
+
+    /**
+     * Modifies the stored value using the provided consumer function.
+     *
+     * @param consumer the function to modify the value
+     *
+     * @return the builder instance for fluent method calls
+     * @throws NullPointerException if the consumer is null
+     */
+    default B modify(Consumer<T> consumer) {
+        Objects.requireNonNull(consumer).accept(getValue());
+        return instance();
+    }
+}
