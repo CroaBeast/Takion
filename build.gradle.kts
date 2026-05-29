2195218b8d0b7411529b763007970f6c2364ae2e
CroaBeast <croabeast@github.com>
2026-05-29 09:53:39 -0500

    chore: update PrismaticAPI to 1.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,6 +79,6 @@ subprojects {
         
         compileOnly("me.croabeast:YAML-API:1.1")
         compileOnly("me.croabeast:GlobalScheduler:1.1")
-        compileOnly("me.croabeast:PrismaticAPI:1.5.0")
+        compileOnly("me.croabeast:PrismaticAPI:1.5.1")
     }
 }
